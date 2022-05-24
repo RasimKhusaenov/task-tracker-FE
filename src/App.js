0394dca39bae32ec8776d0ff26399c17c8f488fd
Rasim Khusaenov <rasim.khusaenov@flatstack.dev>
2022-05-24 17:24:54 +0300

    Update src/App.js
    Co-authored-by: Amir-Gaifullin <47780452+Amir-Gaifullin@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Login from "./pages/Login";
 import Header from "./components/Header";
 import React from "react";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <Header>
