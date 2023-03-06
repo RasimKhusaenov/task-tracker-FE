33b272f1b16df9425b18519d33885ac170aa3253
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2023-03-06 16:06:22 +0300

    Add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  background: black;
+  color: white;
+`;
+
+const Footer = () => {
+  return <StyledFooter>(c) Rasim Khusaenov</StyledFooter>;
+};
+
+export default Footer;
