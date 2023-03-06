9dcecebac237c9b8a30dfd95135e7475ad848f04
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2023-03-06 14:34:13 +0300

    Add global styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import GlobalStyle from './globalStyles';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'reset-css';
@@ -6,6 +7,7 @@ import 'reset-css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
