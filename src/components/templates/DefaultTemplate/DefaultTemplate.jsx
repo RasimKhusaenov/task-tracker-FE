4cabf1d680bbbf829c512b9c2958bbede4a3a732
RasimKhusaenov <rasim.khusaenov@flatstack.dev>
2023-04-02 21:00:29 +0300

    Add "Scroll to top" button

--- src/components/templates/DefaultTemplate/DefaultTemplate.jsx
+++ src/components/templates/DefaultTemplate/DefaultTemplate.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import Header from "../../molecules/Header/Header";
 import Footer from "../../molecules/Footer/Footer";
+import Button from "../../atoms/Button";
 
 const Wrapper = styled.div`
   display: flex;
@@ -12,12 +13,45 @@ const Wrapper = styled.div`
 const MainContent = styled.main`
   flex: 1;
 `;
+const ButtonWrapper = styled.div`
+  margin-top: 2rem;
+  position: fixed;
+  top: 40px;
+  right: 20px;
+`;
 
 const DefaultTemplate = ({ children }) => {
+  const [showScrollButton, setShowScrollButton] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setShowScrollButton(true);
+      } else {
+        setShowScrollButton(false);
+      }
+    };
+
+    document.addEventListener("scroll", handleScroll);
+
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const handleScrollToTop = () => {
+    window.scroll({ top: 0, behavior: "smooth" });
+  };
+
   return (
     <Wrapper>
       <Header />
       <MainContent>{children}</MainContent>
+      {showScrollButton && (
+        <ButtonWrapper>
+          <Button label="Scroll to top" onClick={handleScrollToTop} color="black" />
+        </ButtonWrapper>
+      )}
       <Footer />
     </Wrapper>
   );
